14a24d020fb9ffdeacc83bd8faf0e22a092a5454
Sodamareme <sodadiop065@gmail.com>
2025-04-12 17:07:43 +0000

    first commit

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
-
 // Routes Auth
 router.post('/register', authController.register);
 router.post('/login', authController.login);
